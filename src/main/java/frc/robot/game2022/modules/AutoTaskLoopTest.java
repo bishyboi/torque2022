8a4f0eb4a7576218a2fa95863482a5d4138bab7b
YourPalParanoid <98663544+YourPalParanoid@users.noreply.github.com>
2022-03-04 17:22:32 -0500

    Improved readability :)

--- src/main/java/frc/robot/game2022/modules/AutoTaskLoopTest.java
+++ src/main/java/frc/robot/game2022/modules/AutoTaskLoopTest.java
@@ -1,21 +1,26 @@
 package frc.robot.game2022.modules;
 
-public class AutoTaskLoopTest{
-    public static int[] testLoop(int distance, int phase, int count, int shootingDistance, int exitDistance){
+public class AutoTaskLoopTest
+{
+    public static int[] testLoop(int distance, int phase, int count, int shootingDistance, int exitDistance)
+    {
         int error = 0;
-        switch(phase){
+        switch(phase)
+        {
             case 1: // phase 1: move to shooting location
                 //combine.intakeMove(intakePower);
                 error = shootingDistance-distance;
                 distance = distance + (error/Math.abs(error));//this.centerAlign(shootingDistance, distanceError);
-                if (distance-shootingDistance==0){
+                if (distance-shootingDistance==0)
+                {
                     phase++;
                 }
             break;
             case 2: // phase 2: shoot
                 count++;
                 //combine.intakeMove(-intakePower);
-                if(count >= 100){
+                if(count >= 100)
+                {
                     phase++;
                 }
             break;
@@ -28,13 +33,16 @@ public class AutoTaskLoopTest{
         int[] result = {distance, phase, count, error};
         return result;
     }
-    public static void main (String[] args) throws InterruptedException{
+    public static void main (String[] args) throws InterruptedException
+    {
         int[] data = {100, 1, 0, 0};
         boolean done = false;
-        while (!done){
+        while (!done)
+        {
             data = testLoop(data[0], data[1], data[2], 36, 130);
             System.out.println(data[0]+"\t"+data[1]+"\t"+data[2]+"\t"+data[3]);
-            if(data[1] == 3&&data[3] == 0){
+            if(data[1] == 3&&data[3] == 0)
+            {
                 done = true;
             }
             Thread.sleep(20);
